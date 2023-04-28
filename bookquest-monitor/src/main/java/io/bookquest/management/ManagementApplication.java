209c15deadc09d3ef91ae61ef1d2a55b7ffc6612
SimiaoSamuel <sssimiao10@gmail.com>
2023-04-27 23:53:33 -0300

    feat: jtw auth

--- bookquest-monitor/src/main/java/io/bookquest/management/ManagementApplication.java
+++ bookquest-monitor/src/main/java/io/bookquest/management/ManagementApplication.java
@@ -1,15 +1,52 @@
 package io.bookquest.management;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 @EnableAdminServer
 @SpringBootApplication
 public class ManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
 
+    @Bean
+    public HttpHeadersProvider customHttpHeadersProvider(@Value("${application.security.jwt.secret-key}")
+                                                         String secretKey) {
+        return instance -> {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Authorization", encode("gon", secretKey));
+            return httpHeaders;
+        };
+    }
+
+    protected String encode(String username, String secretKey) {
+        return Jwts
+                .builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(getSignInKey(secretKey), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey(String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
